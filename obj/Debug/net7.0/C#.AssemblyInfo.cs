9cf56ddf918fb0d0e13782f8e83806d5bc91a662
abdulaziz12k <abdullaziz12k@gmail.com>
2024-01-17 11:12:50 +0300

    -

--- obj/Debug/net7.0/C#.AssemblyInfo.cs
+++ obj/Debug/net7.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d112dcd599cc642d4c651b44a1c2a1ce0e1f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
