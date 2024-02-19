2afbbbfbd39085cdff3dc4881de962a454b08183
abdulaziz12k <abdullaziz12k@gmail.com>
2024-02-19 13:32:15 +0300

    -

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ public class Program
         // CalculateGrades();
         // CalculateGPA();
         // GenerateRandomNumber();
-        // SumMethod();
+        SumMethod();
         // DisplayDate();
         // Sortarrays();
         Joinarrays();
@@ -247,6 +247,13 @@ public class Program
         int sum = A + B;
         Console.WriteLine($"the sum of {A} + {B} = {sum}");
         Console.WriteLine("");
+        decimal price = 67.55m;
+        decimal salePrice = 59.99m;
+
+        string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+        yourDiscount += $"A discount of {(price - salePrice) / price:P2}!"; //inserted
+        Console.WriteLine(yourDiscount);
     }
 
     public static void DisplayDate()
